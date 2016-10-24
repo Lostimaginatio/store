c920629928ecb56e2f853ffc3449cf2f8968dec6
Fantasy <ia@linkprofit.ru>
2016-10-24 17:45:51 +0300

    Добавил стилей для таблицы товаров

--- app/models/product_grid.rb
+++ app/models/product_grid.rb
@@ -6,16 +6,16 @@ class ProductGrid
     Spree::Product
   end
 
-  filter(:name, :string, header: 'Наименование товара') do |value, scope|
+  filter(:name, :string, header: 'Наименование') do |value, scope|
     scope.where("name like '%#{value}%'")
   end
   filter(:sku, :string, header: 'Код товара') do |value, scope|
     scope.where("sku like '%#{value}%'")
   end
-  filter(:price, :integer, range: true, header: 'Цена, руб.')
+  filter(:price, :integer, range: true, header: 'Ценовой диапозон')
 
   column(:name, header: 'Наименование товара', :html => true) do |product|
-    link_to product.name, spree.product_url(product.id, @taxon.try(:id))
+    link_to product.name, spree.product_url(product.id, @taxon.try(:id)), class: 'product-name-link'
   end
   column(:sku, header: 'Код товара')
   column(:price, :order => "price", header: 'Цена, руб.')
