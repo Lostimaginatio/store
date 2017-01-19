d790e3c15ec63375b4cb1091d121dcef8ebcaa66
alex <alexey.bazanov13@gmail.com>
2017-01-19 15:59:27 +0300

    last changes

--- db/migrate/20161230181106_add_phone_to_store.rb
+++
@@ -1,5 +0,0 @@
-class AddPhoneToStore < ActiveRecord::Migration
-  def change
-    add_column :spree_stores, :phone_number, :string
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
+
+Spree::Page.create! :title => 'Доставка', :body => '<h1>Информация о доставке</h1>', :slug => '/delivery_info'
+Spree::Page.create! :title => 'О нас', :body => '<h1>Информация о компании</h1>', :slug => '/about_info'
+Spree::Page.create! :title => 'Контакты', :body => '<h1>Информация о контактах</h1>', :slug => '/contacts_info'
+Spree::Page.create! :title => 'Главная нижняя часть', :body => '<h1>Текст сверху</h1>', :slug => '/main_bottom_info'
+Spree::Page.create! :title => 'Главная верхняя часть', :body => '<h1>Текст снизу</h1>', :slug => '/main_top_info'
